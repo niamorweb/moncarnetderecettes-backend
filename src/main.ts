b7cb9c2a5429b09a23c4208c50fe00d6205edc44
Romain <niamor.lth@gmail.com>
2026-01-09 20:54:53 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://moncarnetderecettes.vercel.app'],
+    origin: ['*'],
     credentials: true,
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: '*',
